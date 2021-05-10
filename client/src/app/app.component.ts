056f4c1a80e11ac5cd02509c9ef89c22b55cdcd5
Joey Goertzen <joey.goertzen@gmail.com>
2021-05-09 23:51:32 -0600

    get really simple single player experience going

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
-import { Howl } from 'howler';
+import { Component, OnInit } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 
 @Component({
@@ -9,162 +8,27 @@ import { io, Socket } from 'socket.io-client';
 })
 export class AppComponent implements OnInit {
   
-  state;
-  left;
-  right;
-  choices;
-  score;
-  scoreInterval;
-
-  blockSize = 100;
-  questionCount = 5;
-
-  inputMap = {
-    'j': 0,
-    'k': 1,
-    'l': 2,
-    ';': 3,
-  };
-
-  yepSound: Howl;
-  nopeSound: Howl;
-  youWinSound: Howl;
-  newGameSound: Howl;
-
   socket: Socket;
+  situation;
 
-  constructor(private host: ElementRef) {}
+  constructor() {}
 
   ngOnInit() {
-    this.yepSound = new Howl({ src: ['/assets/yep.flac'] });
-    this.nopeSound = new Howl({ src: ['/assets/nope.flac'] });
-    this.youWinSound = new Howl({ src: ['/assets/youWin.flac'] });
-    this.newGameSound = new Howl({ src: ['/assets/newGame.flac'] });
-    this.createNewGame();
     this.socket = io('http://localhost:3000');
+    this.socket.on('situation update', situation => {
+      this.situation = situation;
+    });
+    this.socket.on('wrong', () => {
+      console.log('wrong!');
+    });
+    this.socket.on('correct', situation => {
+      console.log('correct!');
+      this.situation = situation;
+    });
   }
 
   choose(choice) {
-    this.processInput(choice.value);
+    this.socket.emit('choice submission', choice);
   }
 
-  @HostListener('document:keypress', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
-
-    if(this.state === 'win') {
-      this.createNewGame();
-      return;
-    }
-
-    if(this.state !== 'ask') {
-      return;
-    }
-
-    if(!['j','k','l',';'].includes(event.key)) {
-      return;
-    }
-    
-    const input = this.choices[this.inputMap[event.key]].value;
-    
-    this.processInput(input);
-  }
-
-  processInput(input) {
-    const correct = this.left + this.right === input;
-
-    if(correct) {
-      this.setScore(this.score + 20);
-
-      if(this.score === 100) {
-        this.state = 'win';
-        clearInterval(this.scoreInterval);
-        this.youWinSound.play();
-      } else {
-        this.state = 'correct';
-        this.yepSound.play();
-        setTimeout(() => this.createNewQuestion(), 500);
-      }
-    } else {
-      this.setScore(this.score - 10);
-      this.state = 'incorrect';
-      this.nopeSound.play();
-      setTimeout(() => this.state = 'ask', 500);
-    }
-  }
-
-  setScore(score) {
-    this.score = Math.min(Math.max(score, 0), 100);
-    this.host.nativeElement.style.setProperty('--score', `${this.score}%`);
-  }
-
-  createNewGame() {
-    this.newGameSound.play();
-    this.setScore(0);
-    this.scoreInterval = setInterval(() => this.setScore(this.score - 1), 100);
-    this.createNewQuestion();
-  }
-
-  createNewQuestion() {
-    this.state = 'ask';
-    this.left = this.getNumber();
-    this.right = this.getNumber();
-    this.choices = this.getChoices(this.left + this.right);
-  }
-
-  getChoices(answer) {
-    const values = [answer];
-    
-    for(let i = 0; i < 3; ++i) {
-      let decoy = answer;
-
-      while(values.includes(decoy)) {
-        decoy = this.getDecoy(answer);
-      }
-
-      values.push(decoy);
-    }
-
-    this.shuffle(values);
-
-    const choices = values.map(value => {
-      let choice: IChoice = {value};
-      return choice;
-    });
-
-    choices[0].key = 'j';
-    choices[1].key = 'k';
-    choices[2].key = 'l';
-    choices[3].key = ';';
-
-    return choices;
-  }
-
-  shuffle(list) {
-    for(let i = 0; i < 20; ++i) {
-      const a = Math.floor(Math.random() * list.length);
-      const b = Math.floor(Math.random() * list.length);
-      const temp = list[a];
-      list[a] = list[b];
-      list[b] = temp;
-    }
-  }
-
-  getDecoy(answer) {
-    let offset = 1 + Math.floor(Math.random() * 5);
-    
-    if(Math.random() > 0.5) {
-      offset *= -1;
-    }
-
-    return answer + offset;
-  }
-
-  getNumber() {
-    return Math.floor(Math.random() * 10);
-  }
-}
-
-interface IChoice {
-  value: number;
-  key?: string;
 }
--- server/index.js
+++ server/index.js
@@ -12,19 +12,58 @@ const io = new Server(httpServer, {
   }
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+///////////////////////////////////
+
+let questions;
+const players = {};
+
+function generateQuestions() {
+  questions = [
+    { text: '3 + 3', answer: 6, choices: [3, 6, 2, 1] },
+    { text: '1 + 8', answer: 9, choices: [2, 3, 9, 0] },
+    { text: '1 + 1', answer: 2, choices: [2, 1, 3, 4] },
+  ]
+}
+
+generateQuestions();
 
 io.on('connection', socket => {
-  console.log('a user connected');
+  
+  const player = {
+    socket,
+    position: 0,
+  };
+  
+  players[socket.id] = player;
+  
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    delete players[socket.id];
   });
-  socket.on('chat message', msg => {
-    console.log('message: ' + msg);
-    // io.emit('chat message', msg);
+
+  socket.on('choice submission', choice => {
+    const question = questions[player.position];
+    if(choice === question.answer) {
+      player.position += 1;
+      socket.emit('correct', {
+        question: questions[player.position],
+        position: player.position,
+      });
+    } else {
+      socket.emit('wrong');
+    }
   });
+
+  socket.emit('situation update', { 
+    question: questions[player.position],
+    position: player.position,
+    questionCount: questions.length,
+  });
+});
+
+//////////////////////////////////////////////////////
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 httpServer.listen(3000, () => {
