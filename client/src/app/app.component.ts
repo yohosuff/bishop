f1016cd5ee8c0d906e647769327b32c88a29e812
Joey Goertzen <joey.goertzen@gmail.com>
2021-05-13 22:30:38 -0600

    add 'addPlayer' debug shortcut

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { EventName } from '../../../server/src/event-name';
 import { StateName } from './state-name';
@@ -16,12 +16,25 @@ export class AppComponent implements OnInit {
   questionCount;
   state: string;
   winner;
-  players: Map<string, any>;
-  playersIterable: any[];
+  playersMap: Map<string, any>;
+  players: any[];
 
   StateName = StateName;
 
-  constructor() {}
+  constructor(private host: ElementRef) {}
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    console.log(event);
+    if(event.code === 'Numpad1') {
+      const name = `player${this.players.length + 1}`;
+      this.addPlayer({
+        name,
+        id: name,
+        position: 0,
+      });
+    }
+  }
 
   ngOnInit() {
     this.state = StateName.WAIT;
@@ -44,9 +57,9 @@ export class AppComponent implements OnInit {
 
     socket.on(EventName.WELCOME, params => {
       console.log('welcome', params);
-      this.players = (params.players as any[]).reduce((p,c) => p.set(c.id, c), new Map<string, any>());
-      this.playersIterable = params.players;
-      this.me = this.players.get(params.id);
+      this.players = params.players;
+      this.playersMap = params.players.reduce((p, c) => p.set(c.id, c), new Map<string, any>());
+      this.me = this.playersMap.get(params.id);
       this.question = params.question;
       this.questionCount = params.questionCount;
       this.state = StateName.PLAY;
@@ -64,7 +77,7 @@ export class AppComponent implements OnInit {
 
     socket.on(EventName.UPDATE, params => {
       console.log('update', params);
-      const player = this.players.get(params.player.id);
+      const player = this.playersMap.get(params.player.id);
       player.position = params.player.position;
     });
 
@@ -78,24 +91,28 @@ export class AppComponent implements OnInit {
       console.log('loser');
       this.winner = params.winner;
       Array.from(params.players.values()).forEach((player: any) => {
-        this.players.get(player.id).position = player.position;
+        this.playersMap.get(player.id).position = player.position;
       });
       this.state = StateName.LOSE;
     });
 
     socket.on(EventName.JOINED, params => {
       console.log('joined', params);
-      this.players.set(params.player.id, params.player);
-      this.playersIterable = Array.from(this.players.values());
+      this.addPlayer(params.player);
     });
 
     socket.on(EventName.LEFT, params => {
       console.log('left', params);
-      this.players.delete(params.player.id);
-      this.playersIterable = Array.from(this.players.values());
+      this.playersMap.delete(params.player.id);
+      this.players = Array.from(this.playersMap.values());
     });
 
     return socket;
+  }
+
+  addPlayer(player) {
+    this.playersMap.set(player.id, player);
+    this.players = Array.from(this.playersMap.values());
   }
 
 }
